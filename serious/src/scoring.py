de710bc024d60d7a145b126876e54ef2940394f5
Gurvan Cabon <gurvan.cabon@inria.fr>
2017-02-23 20:00:14 +0100

    fixed a bug

--- serious/src/scoring.py
+++ serious/src/scoring.py
@@ -7,7 +7,7 @@ def score(problem,solution) :
         video = resquest.vid                                                                  # the video to stream
         endpoint = request.endpoint                                                           # the endpoint where to stream
         number = request.n                                                                    # number of time the video is streamed
-        servers_with_video = filter(lambda s: v in s.videos, endpoints.servers.keys())        # keeping only the servers containg the video
+        servers_with_video = filter(lambda s: v in solution.s, endpoints.servers.keys()) # keeping only the servers containg the video
         l = min(endpoint.latency,min(map(lambda s: endpoint.servers[s],servers_with_video)))  # best latency
         timeSaved += number * l                                                               # adding to the total time saved
     return (1000*timeSaved/(len(problem.requests)))                                           # returning average time saved
