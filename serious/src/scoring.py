2698886c1181560ba949470572c8aff6df08b0c4
Gurvan Cabon <gurvan.cabon@inria.fr>
2017-02-23 20:18:27 +0100

    another bug

--- serious/src/scoring.py
+++ serious/src/scoring.py
@@ -1,13 +1,14 @@
 from datas import *
 
 def score(problem,solution) :
-    timeSaved = 0
+    timeSaved,total = 0,0
     for request in problem.requests:
     #for each request, we calculat the time saved
         video = resquest.vid                                                                  # the video to stream
         endpoint = request.endpoint                                                           # the endpoint where to stream
         number = request.n                                                                    # number of time the video is streamed
-        servers_with_video = filter(lambda s: v in solution.s, endpoints.servers.keys()) # keeping only the servers containg the video
+        servers_with_video = filter(lambda s: v in solution.s, endpoints.servers.keys())      # keeping only the servers containg the video
         l = min(endpoint.latency,min(map(lambda s: endpoint.servers[s],servers_with_video)))  # best latency
         timeSaved += number * l                                                               # adding to the total time saved
-    return (1000*timeSaved/(len(problem.requests)))                                           # returning average time saved
+        total += number 
+    return (1000*timeSaved/total)                                           # returning average time saved
