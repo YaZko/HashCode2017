fac4d704819c02caa5ed4520811dfc923ebe4d7d
Yaya <y.zakowski@gmail.com>
2017-02-23 19:03:48 +0100

    Datastructures

---
+++ serious/src/datas.py
@@ -0,0 +1,38 @@
+class Problem():
+
+    def __init__(self,c,ns,ne,e,nv,v,nr,r):
+
+        self.capacity = c     # Capacity of each server
+        self.nb_servers = ns  # Number of cache servers
+
+        self.nb_endpoints = ne  # Number of endpoints
+        self.endpoints = e      # List of endpoints
+
+        self.nb_vids = nv     # Number of videos
+        self.vids = v         # List of videos
+
+        self.nb_requests = nr # Number of requests
+        self.requests = r     # List of requests
+
+class Request():
+
+    def __init__(self,v,e,n):
+        self.vid = v         # Video requested
+        self.endpoint = e    # endpoint requesting
+        self.n = n           # Number of requests
+
+class Video():
+
+    def __init__(self,s):
+        self.size = s     # Size of the video
+        self.servers = [] # List of servers in which one the video is cached
+
+class EndPoint():
+
+    def __init__(self,t,id,d):
+
+        self.latency = t  # Latency to the data center
+        self.id      = id
+        self.servers = d  # A dictionnary mapping connected servers to their latency
+
+
