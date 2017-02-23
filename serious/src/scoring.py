0148adf2feee38a8c75ff47df0d234c90d1353e9
Gurvan Cabon <gurvan.cabon@inria.fr>
2017-02-23 19:51:07 +0100

    fixed a bug

--- serious/src/scoring.py
+++ serious/src/scoring.py
@@ -4,9 +4,10 @@ def score(problem,solution) :
     timeSaved = 0
     for request in problem.requests:
     #for each request, we calculat the time saved
-        video = resquest.vid                                           # the video to stream
-        endpoint = request.endpoint                                    # the endpoint where to stream
-        number = request.n                                             # number of time the video is streamed
-        l = min(endpoint.latency,min(list(endpoint.servers.values()))) # best latency
-        timeSaved += number * l
-    return (1000*timeSaved/(len(problem.requests)))
+        video = resquest.vid                                                                  # the video to stream
+        endpoint = request.endpoint                                                           # the endpoint where to stream
+        number = request.n                                                                    # number of time the video is streamed
+        servers_with_video = filter(lambda s: v in s.videos, endpoints.servers.keys())        # keeping only the servers containg the video
+        l = min(endpoint.latency,min(map(lambda s: endpoint.servers[s],servers_with_video)))  # best latency
+        timeSaved += number * l                                                               # adding to the total time saved
+    return (1000*timeSaved/(len(problem.requests)))                                           # returning average time saved
