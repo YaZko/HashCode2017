901ba845ddc4d21c4585d0dd812e3232ef34069e
Yaya <y.zakowski@gmail.com>
2017-02-23 20:27:32 +0100

    solver

---
+++ serious/src/solve.py
@@ -0,0 +1,34 @@
+from datas import *
+from parser import *
+from scoring import *
+
+def get_req(pb):
+
+    # We initialize a dictionnary mapping servers to dictionnaries mapping vids to number of accessible requests
+    d = {s:{v:0 for v in range(pb.nb_vids)} for s in range(pb.nb_servers)}
+    for r in pb.requests:
+        e = r.endpoint
+        v = r.vid
+        n = r.n
+        for s in e.servers:
+            d[s][v.id] = d[s][v.id] + r.n
+
+    # for a,b in d.items():
+    #     print("{}: {}".format(a,b))
+
+    sol = [[] for s in range(pb.nb_servers)]
+
+    for s in range(pb.nb_servers):
+        # We sort the videos for each server by ratio (number of requests)/(size of videos)
+        l = sorted(d[s].items(),key=lambda x: x[1]/pb.vids[x[0]].size,reverse=True)
+        for v,n in l:
+            # print(pb.servers[s].load + pb.vids[v].size)
+            if pb.servers[s].load + pb.vids[v].size <= pb.capacity:
+                sol[s].append(v)
+                pb.servers[s].load += pb.vids[v].size
+
+    return sol
+
+pb = parse("../inputs/example.in")
+print(score(pb,get_req(pb)))
+# print((parse("../inputs/example.in")))
